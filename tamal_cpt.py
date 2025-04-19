c63babac7f419ca19a822e90ce73952c9e9769ce
sohiniroybrandeis <150547559+sohiniroybrandeis@users.noreply.github.com>
2025-04-18 21:22:02 -0600

    Current CER=0.26. Trying different batch size.

--- tamal_cpt.py
+++ tamal_cpt.py
@@ -218,10 +218,10 @@ training_args = TrainingArguments(
 		output_dir='pretraining-res-tammal',
 		gradient_checkpointing=True, 
 		group_by_length=True,   # groups examples of comparable lengths together
-		gradient_accumulation_steps=2,
-		per_device_eval_batch_size=2,
+		gradient_accumulation_steps=1,
+		per_device_eval_batch_size=4,
 		num_train_epochs=5,
-		per_device_train_batch_size=2,
+		per_device_train_batch_size=4,
         max_grad_norm=1.0,  
 		
 		# logging...
