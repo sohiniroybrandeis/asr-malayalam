da20c02ecbeca6fafdf5029be01528ab6fe316a5
sohiniroybrandeis <150547559+sohiniroybrandeis@users.noreply.github.com>
2025-04-19 20:24:25 -0600

    Changing checkpoint

--- topline_cpt.py
+++ topline_cpt.py
@@ -311,7 +311,7 @@ tokenizer = Wav2Vec2CTCTokenizer.from_pretrained("./", unk_token="[UNK]", pad_to
 repo_name = "cpt3-wav2vec2-large-xls-r-300m-mal30-results"
 tokenizer.save_pretrained(repo_name)
 
-feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained("pretraining-res-mal30/checkpoint-47600")
+feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained("pretraining-res-mal30/checkpoint-24085")
 processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
 mal_data_train = mal_data_train.cast_column("audio", Audio(sampling_rate=16_000))
@@ -407,7 +407,7 @@ def compute_metrics(pred):
 
 
 model = Wav2Vec2ForCTC.from_pretrained(
-    "pretraining-res-mal30/checkpoint-47600", 
+    "pretraining-res-mal30/checkpoint-24085", 
     attention_dropout=0.0,
     hidden_dropout=0.0,
     feat_proj_dropout=0.0,
