cf9e3a83aaf3ae902a13af1bdfb2ec9203aec452
sohiniroybrandeis <150547559+sohiniroybrandeis@users.noreply.github.com>
2025-04-26 19:31:44 -0400

    Create atds.sh

---
+++ atds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=atds
+
+#SBATCH --output=atds_results.txt
+
+#SBATCH --gres=gpu:1       # Request 1 GPU (adjust if needed)
+
+#SBATCH --ntasks=4         # Use 4 CPU tasks (for data loading)
+
+#SBATCH --mem=32G          # Request 32GB total RAM
+
+hostname
+python3 atds.py
