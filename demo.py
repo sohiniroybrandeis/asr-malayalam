1c62832cf96ccff547b60d927dd91b5555f599ed
sohiniroybrandeis <150547559+sohiniroybrandeis@users.noreply.github.com>
2025-04-27 19:22:15 -0400

    Tried cpt1 and cer=0.381. Trying cp2

--- demo.py
+++ demo.py
@@ -5,7 +5,8 @@ from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 from jiwer import cer
 
 # 1. Get model, audio file, and reference transcription from command-line
-model_dir = "cpt1-wav2vec2-large-xls-r-300m-malayalam-results/checkpoint-1950"
+# model_dir = "cpt1-wav2vec2-large-xls-r-300m-malayalam-results/checkpoint-1950"
+model_dir = "cpt2-wav2vec2-large-xls-r-300m-tammal-results/checkpoint-1950"
 audio_path = "demo/sonia_set1_21.wav"
 reference_text = "മെഡിക്കൽ ലബോറട്ടറി ടെക്നോളജി റഗുലർ നവംബർ രണ്ടായിരത്തി പതിനാല് പരീക്ഷാഫലം വെബ്സൈറ്റിൽ"  # Give the correct transcription as an argument
 
