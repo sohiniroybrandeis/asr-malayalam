3ad256f2980dad065bf7054c12f6ae7c70c1a146
sohiniroybrandeis <150547559+sohiniroybrandeis@users.noreply.github.com>
2025-04-27 19:18:36 -0400

    Typo

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ from jiwer import cer
 
 # 1. Get model, audio file, and reference transcription from command-line
 model_dir = "cpt1-wav2vec2-large-xls-r-300m-malayalam-results/checkpoint-1950"
-audio_path = "sonia_set1_21.wav"
+audio_path = "demo/sonia_set1_21.wav"
 reference_text = "മെഡിക്കൽ ലബോറട്ടറി ടെക്നോളജി റഗുലർ നവംബർ രണ്ടായിരത്തി പതിനാല് പരീക്ഷാഫലം വെബ്സൈറ്റിൽ"  # Give the correct transcription as an argument
 
 # 2. Load model and processor
