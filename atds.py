bfbd3b033223e7e9cb814c323c4e7d3b1f5ab009
sohiniroybrandeis <150547559+sohiniroybrandeis@users.noreply.github.com>
2025-05-03 18:24:18 -0400

    Made a typo

--- atds.py
+++ atds.py
@@ -49,25 +49,6 @@ print("Total duration: ", total_duration_k)
 
 telugu_dataset = Dataset.from_list(selected_samples_k)
 
-# # Load the Tamil data
-# tamil_dataset = load_from_disk("tammal_IS_audio_dataset")
-
-# # Compute durations
-# tamil_dataset = tamil_dataset.map(compute_durations, batched=True)
-
-# selected_samples_t = []
-# total_duration_t = 0.0
-
-# for sample in tamil_dataset:
-#     if total_duration_t + sample["duration"] > (3600 * 3.75): #3.75 hours
-#         break
-#     selected_samples_t.append(sample)
-#     total_duration_t += sample["duration"]
-    
-# print("Total duration: ", total_duration_t)
-
-# tamil_dataset = Dataset.from_list(selected_samples_t)
-
 # 2. Load pretrained Wav2Vec2 model
 model_name = "facebook/wav2vec2-xls-r-300m"  # or a model trained on Malayalam
 feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
@@ -114,19 +95,13 @@ def build_token_frequency(dataset, sample_size=500):
 # 5. Compute frequency vectors
 malayalam_freq = build_token_frequency(malayalam_dataset)
 telg_freq = build_token_frequency(telugu_dataset)
-# tamil_freq = build_token_frequency(tamil_dataset)
 
 # Pad the shorter vector (make same length)
 max_len = max(len(malayalam_freq), len(telg_freq))
 malayalam_freq = np.pad(malayalam_freq, (0, max_len - len(malayalam_freq)))
-tamil_freq = np.pad(telg_freq, (0, max_len - len(telg_freq)))
-# max_len = max(len(malayalam_freq), len(tamil_freq))
-# malayalam_freq = np.pad(malayalam_freq, (0, max_len - len(malayalam_freq)))
-# tamil_freq = np.pad(tamil_freq, (0, max_len - len(tamil_freq)))
+telg_freq = np.pad(telg_freq, (0, max_len - len(telg_freq)))
 
 # 6. Cosine similarity
 similarity = cosine_similarity([malayalam_freq], [telg_freq])[0][0]
-# similarity = cosine_similarity([malayalam_freq], [tamil_freq])[0][0]
 
 print(f"ATDS (Acoustic Token Distribution Similarity) between Malayalam and Telugu: {similarity:.4f}")
-# print(f"ATDS (Acoustic Token Distribution Similarity) between Malayalam and Tamil: {similarity:.4f}")
